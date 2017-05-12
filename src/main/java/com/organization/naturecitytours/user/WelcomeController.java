6018d694b76f5c88aaec324d684f16f29168d06b
avalcarcel <avalcarc@jaumebalmes.net>
2017-05-12 18:06:52 +0200

    Update WelcomeController.java

--- src/main/java/com/organization/naturecitytours/user/WelcomeController.java
+++ src/main/java/com/organization/naturecitytours/user/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 class WelcomeController {
 
-     @RequestMapping("/")
+     @RequestMapping("/hola")
     public String welcome() {
         return "world";
     }
