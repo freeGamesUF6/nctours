870018589b627ecdcff978aac13113c9e8487d69
xavibana <xavibana@gmail.com>
2017-05-18 04:07:18 +0200

    Xavier Baños Arreglando MAPEOS! Añadida Libreria JQuery al Head 18/05/2017 04:07h

--- src/main/java/com/organization/naturecitytours/book/Bookuser.java
+++ src/main/java/com/organization/naturecitytours/book/Bookuser.java
@@ -5,11 +5,13 @@
  */
 package com.organization.naturecitytours.book;
 
+import com.organization.naturecitytours.book.Bookuser.BookuserId;
 import com.organization.naturecitytours.user.User;
 import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -17,9 +19,11 @@ import javax.persistence.Table;
 /**
  *
  * @author Mark
+ * @author Xavier
  */
 @Entity
 @Table(name="bookuser")
+@IdClass(BookuserId.class)
 public class Bookuser implements Serializable {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name="iduser")
@@ -32,8 +36,6 @@ public class Bookuser implements Serializable {
 
     public Bookuser() {
     }
-    
-    
 
     public User getIduser() {
         return iduser;
@@ -49,6 +51,18 @@ public class Bookuser implements Serializable {
 
     public void setIdbook(Book idbook) {
         this.idbook = idbook;
+    }
+
+    
+    /**
+     * Mark aixó és el que te a veure amb el IdClass, he ficat LONG pq es el que he trobat a l'exemple d'internet, pero està clar que s'ha de posar altre cosa! això ja t'ho deixo a tu q me embolio!
+     * adjunto web d'on he tret solucions!
+     * http://stackoverflow.com/questions/38613755/java-lang-illegalargumentexception-expecting-idclass-mapping
+     * 
+     */
+    public class BookuserId implements Serializable {
+        private Long iduser;
+        private Long idbook;
     }
     
     
